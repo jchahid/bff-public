7fe6af90623d6bed81ffa409fce159077cd0f17f
jchahid <jalal.chahid.86@gmail.com>
2025-09-06 10:42:53 +0200

    first commit

---
+++ src/main/java/fr/jcd/bffpublic/BffPublicApplication.java
@@ -0,0 +1,13 @@
+package fr.jcd.bffpublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffPublicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffPublicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/jcd/bffpublic/BffPublicApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.jcd.bffpublic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffPublicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
